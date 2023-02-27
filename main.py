c9470ccb8b079d534f8819f8460f998f6903be4d
ashshelby <nepalidude3@gmail.com>
2023-02-27 21:45:51 +0545

    got all endpoints

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     uncensored = EverestUncensored(os.path.join(os.getcwd(),"..","./driver/geckodriver-0.32.2/target/release/geckodriver"),"link_file.txt")
     try:
         uncensored.read_link()
-        uncensored.get_all_links()
+        # uncensored.get_all_links()
         uncensored.create_driver()
-        uncensored.go_to_individual_page()
+        # uncensored.get_hike_information()
     except KeyboardInterrupt:
         print("Exiting Gratefully")
         uncensored.exit()
