8d10f60f4cc06acf46111606f6f15a41938ceebf
ashshelby <nepalidude3@gmail.com>
2023-04-11 01:31:01 +0545

    created final works for all version

---
+++ everest_uncensored.py
@@ -0,0 +1,64 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+
+
+class Scraper():
+    """ Web Scraper for the Everest Uncensored Website"""
+
+    def __init__(self, url: str):
+        self.url: str = url
+        self.allowed_extensions = ["jpg", "png"]
+        self.download_path = os.path.join(os.getcwd(),self.url.split("/")[-1])
+
+    def get_html(self) -> str:
+        """Returns HTML Content of the required page."""
+        return requests.get(self.url, timeout=30).text
+
+    def soup(self, html_content) -> BeautifulSoup:
+        """Returns the BeautifulSoup Object based on raw html provided of the website"""
+        return BeautifulSoup(html_content, "lxml")
+
+    def get_links(self, soup: BeautifulSoup) -> list:
+        """Returns all images """
+        links = []
+        for link in soup.findAll("img"):
+            if "amazonaws" in link.get("src"):
+                links.append(link.get("src"))
+
+        return links
+
+    def create_new_directory(self) -> None:
+
+        """Creates a new directory to store the images"""
+        try:
+            os.mkdir(self.download_path)
+        except FileExistsError:
+            print("Folder Already exists, formatting and re-populating!")
+            os.rmdir(self.download_path)
+            os.mkdir(self.download_path)
+
+    def download_images(self,links:list)->None:
+        """Downloads the image and sends the obtained binary data to write to file function """
+        for link in links:
+            data = requests.get(link,stream=True,timeout=30)
+            print(f"[x] Got Image from Link {link}")
+            self.write_to_file(data,link.split("/")[-1])
+
+    def write_to_file(self,data:bytes,name:str)->None:
+        """Writes Image Binary Data to File"""
+        with open(os.path.join(self.download_path,name),"wb") as writer:
+            writer.write(data.content)
+
+    def scrape(self):
+        """Main Method, Run to Scrape"""
+        html_content = self.get_html()
+        links = self.get_links(self.soup(html_content))
+        self.create_new_directory()
+        self.download_images(links)
+
+
+
+eu = Scraper(
+    "https://everestuncensored.deerhold.com/hiking-tokha-budanilkantha")
+eu.scrape()
---
+++ run.py
@@ -0,0 +1,12 @@
+from sys import argv
+from everest_uncensored import Scraper
+
+if len(argv) != 2:
+    raise Exception("Need a link \nUse:\npython run.py https://everestuncensored.deerhold.com/jamacho-hike-2005")
+
+if(argv[1][:5] != "https"):
+    raise Exception("Passed Argument must be a link")
+
+print(argv)
+scraper = Scraper(argv[1])
+scraper.scrape()
